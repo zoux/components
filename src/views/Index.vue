15623ee2f34b2aa9d94823dd2d53ac11166d00cd
zouxin <zouxin@iboxpay.com>
2019-01-16 15:57:48 +0800

    init

---
+++ src/views/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="index">
+    <div class="nav">
+      <router-link to="/scroll">Scroll</router-link> |
+      <!--<router-link to="/about">About</router-link>-->
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .nav {
+    padding: 30px;
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+      &.router-link-exact-active {
+        color: #42b983;
+      }
+    }
+  }
+</style>
